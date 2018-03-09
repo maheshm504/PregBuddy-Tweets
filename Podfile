1f490e3fd3f25ee274825a182f7cb6902a2e72c1
maheshm504 <maheshmasetti.ios@gmail.com>
2018-03-09 17:20:54 +0530

    clear all

--- Podfile
+++
@@ -1,8 +0,0 @@
-#platform :ios, '10.0'
-
-target 'PregBuddy Tweets' do
-
-  use_frameworks!
-  pod 'TwitterKit'
-
-end
--- Pods/Target Support Files/Pods-PregBuddy Tweets/Pods-PregBuddy Tweets-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PregBuddy_Tweets : NSObject
-@end
-@implementation PodsDummy_Pods_PregBuddy_Tweets
-@end
