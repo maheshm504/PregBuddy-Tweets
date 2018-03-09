9d44c8e4ee868721a16c40acb2545673738c5ee9
maheshm504 <maheshmasetti.ios@gmail.com>
2018-03-09 17:16:36 +0530

    Tweets completed

---
+++ Podfile
@@ -0,0 +1,8 @@
+#platform :ios, '10.0'
+
+target 'PregBuddy Tweets' do
+
+  use_frameworks!
+  pod 'TwitterKit'
+
+end
---
+++ PregBuddy Tweets/PregBuddyConstants.swift
@@ -0,0 +1,13 @@
+//
+//  PregBuddyConstants.swift
+//  PregBuddy Tweets
+//
+//  Created by CBH iOS on 09/03/18.
+//  Copyright © 2018 MM iOS. All rights reserved.
+//
+
+import Foundation
+
+let TWTRConsumerKey = "5HTs1OnDnmqzG4UMEpIAdgunE"
+let TWTRConsumerSecret = "f5W04cN2mxKNo48VRVRcJ3qH9CA3zsjzbTlarEfVdCFxlZVbDp"
+
